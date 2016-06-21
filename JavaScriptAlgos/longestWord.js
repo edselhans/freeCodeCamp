28857c05df634484519abb187444c9dd88565632
Ian Trancynger <itrancynger@gmail.com>
2016-06-21 18:11:03 -0400

    Update longestWord.js

--- JavaScriptAlgos/longestWord.js
+++ JavaScriptAlgos/longestWord.js
@@ -1,5 +1,21 @@
+//First Iteration -EAU
 function findLongestWord(str) {
   return str.split(" ").sort(function(a, b) {
     return b.length - a.length;
   })[0].length;
+}
+
+//Second Iteration - IT
+function findLongestWord(str) {
+  var array = [];
+  var lengthy = [];
+  array = str.split(' ');
+  for (i = 0;  i < array.length; i++) {
+    lengthy[i] = array[i].length;
+  }
+  lengthy.sort(function(a, b){
+    return b - a;
+  });
+  lengthy.slice(0,1).join('');
+  return parseInt(lengthy);
 }
